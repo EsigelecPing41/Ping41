a210c97d3deee5020ab16c33e17b41bed29ef64f
antoineLeproust <a.leproust.10@groupe-esigelec.fr>
2013-01-17 14:35:02 +0100

    Surcharge et sans CB

--- src/Modele/Piece.java
+++ src/Modele/Piece.java
@@ -16,11 +16,9 @@ public class Piece
     	this.P_A_Nom=A_Nom;
     	this.P_Statut = statut;
     }
-    public Piece(String nom, String CB, String A_Nom, boolean statut)
+    public Piece(String nom, String A_Nom, boolean statut)
     {
-    
-    	this.P_Nom = nom;
-    	this.P_CodeBarre = CB;
+       	this.P_Nom = nom;
     	this.P_A_Nom=A_Nom;
     	this.P_Statut = statut;
     }
