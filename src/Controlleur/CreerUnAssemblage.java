f2f18e4995f75ba6ef28a15de1f7edeca3222edf
guillaumeCrochemore <g.crochemore.10@groupe-esigelec.fr>
2013-01-20 15:56:45 +0100

    Creer un assemblage

---
+++ src/Controlleur/CreerUnAssemblage.java
@@ -0,0 +1,51 @@
+/*****************************************************
+* Module : Controlleur
+* Fichier : LancerListeAServir
+* Description : 
+* Projet : SKF Traceability
+* Auteur : GC
+* Date : 12/12/12
+* Version : 0.5
+******************************************************/
+
+package Controlleur;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LancerListeAServir
+ */
+@WebServlet(description = "Création d'un assemblage", urlPatterns = { "/CreerUnAssemblage" })
+public class CreerUnAssemblage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreerUnAssemblage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("servlet : On créer un assemblage");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("servlet : On créer un assemblage");
+	}
+
+}
