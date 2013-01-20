14649273e3a7df49dd1354e4b4b972669c03a101
antoineLeproust <a.leproust.10>
2013-01-20 20:54:31 +0100

    Ajout de Assemblage.java

--- src/Modele/Date.java
+++ src/Modele/Date.java
@@ -1,15 +1,22 @@
 package Modele;
 
 import java.util.Calendar;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class Date {
   public static final String date = "yyyy-MM-dd HH:mm:ss";
 
-  public static String now() {
+  public static java.util.Date now() {
   Calendar calendar = Calendar.getInstance();
   SimpleDateFormat dateFormat = new SimpleDateFormat(date);
-  return dateFormat.format(calendar.getTime());
+  try {
+	return dateFormat.parse(String.valueOf(calendar.getTime()));
+} catch (ParseException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+return null;
 }
   public static void  main(String arg[]) {
   System.out.println("Date and Time : " + Date.now());
